406865f63c79bcdc2b58b2898dd1d5c0b8ff25bb
Wandalen <wandalen.me@gmail.com>
2018-08-26 15:58:33 +0300

    auto commit

--- sample/Sample.js
+++ sample/Sample.js
@@ -4,18 +4,22 @@ require( 'wweburifundamentals' );
 var _ = wTools;
 
 var uri = 'complex+protocol://www.site.com:13/path/name?query=here&and=here#anchor';
-var parsed = _.uri.uriParse( uri );
+var parsed = _.weburi.parse( uri );
 console.log( 'parsed :', parsed );
 
 /*
-parsed : { protocol: 'complex+protocol',
+parsed :
+{
+  protocol: 'complex+protocol',
   host: 'www.site.com',
   port: '13',
   localPath: '/path/name',
   query: 'query=here&and=here',
   hash: 'anchor',
+  longPath: 'www.site.com:13/path/name',
   protocols: [ 'complex', 'protocol' ],
   hostWithPort: 'www.site.com:13',
   origin: 'complex+protocol://www.site.com:13',
-  full: 'complex+protocol://www.site.com:13/path/name?query=here&and=here#anchor' }
+  full: 'complex+protocol://www.site.com:13/path/name?query=here&and=here#anchor'
+}
 */
