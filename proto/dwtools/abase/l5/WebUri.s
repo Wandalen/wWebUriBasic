11fbb3312d49efa4677054087ae21bfdafad6576
Wandalen <wandalen.me@gmail.com>
2018-11-05 17:04:06 +0200

    auto

--- proto/dwtools/abase/l5/WebUri.s
+++ proto/dwtools/abase/l5/WebUri.s
@@ -186,7 +186,7 @@ Self.Init();
 
 if( typeof module !== 'undefined' )
 if( _global_.WTOOLS_PRIVATE )
-delete require.cache[ module.id ];
+{ /* delete require.cache[ module.id ]; */ }
 
 if( typeof module !== 'undefined' && module !== null )
 module[ 'exports' ] = Self;
